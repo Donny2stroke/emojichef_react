a6a90b2799fafaa5619c3a9c76c5492dd1a39c09
Claudio D'Onofrio <claudio2000@hotmail.it>
2023-06-12 22:27:19 +0200

    Stato bottone enabled/disabled

--- src/view/Card/Card.js
+++ src/view/Card/Card.js
@@ -7,8 +7,7 @@ import './Card.css'
 function Card(props) {
 
   const contextValue = useContext(AppContext);
-  const [numSelezionatiCorrenti, setNumSelezionatiCorrenti] = useState();
-  
+    
   function selezionaCard(e, contextValue){
       var cardCliccata = e.target;
       var classListCardCliccata = cardCliccata.classList;
@@ -16,30 +15,24 @@ function Card(props) {
       if(classListCardCliccata.contains('checked')){
         contextValue.statoSelezionati.setIngredientiSelezionati(contextValue.statoSelezionati.ingredientiSelezionati-1)
         classListCardCliccata.remove('checked');
-        console.log(numSelezionatiCorrenti);
-        setNumSelezionatiCorrenti(numSelezionatiCorrenti-1)
       }else{
         if(contextValue.statoSelezionati.ingredientiSelezionati < 3){
           contextValue.statoSelezionati.setIngredientiSelezionati(contextValue.statoSelezionati.ingredientiSelezionati+1)
           classListCardCliccata.add('checked');
-          console.log(numSelezionatiCorrenti);
-          setNumSelezionatiCorrenti(numSelezionatiCorrenti+1)
         }else{
           alert("non puoi selezionare più di 3 ingredienti");
         }
       }
   }
   
-  useEffect(()=>{
-    console.log(numSelezionatiCorrenti);
-    if(numSelezionatiCorrenti<3){
-      console.log("minore");
+  useEffect(() => {  
+    if(contextValue.statoSelezionati.ingredientiSelezionati<3){
       contextValue.statoBottone.setBottoneDisabilitato(true);
     }else{
-      console.log("uguale");
       contextValue.statoBottone.setBottoneDisabilitato(false);
     }
-  }, [numSelezionatiCorrenti])
+  }, [contextValue.statoSelezionati.ingredientiSelezionati])
+  
   
   return (
     <div onClick={(e)=>selezionaCard(e, contextValue)} className="card">
